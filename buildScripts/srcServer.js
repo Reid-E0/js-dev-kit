7c92fcab03c84df21211c67917aa7161ac0540d2
Reid E <loess_sobs0n@icloud.com>
2024-06-27 11:09:02 -0400

    added eslint, updated node scripts, and added local tunnel

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -3,7 +3,7 @@ import path from 'path';
 import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
-
+/* eslint-disable no-console */
 const port = 3000;
 const app = express();
 const compiler = webpack(config);
@@ -13,7 +13,6 @@ app.use(
     publicPath: config.output.publicPath,
   })
 );
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
